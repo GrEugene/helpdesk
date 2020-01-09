5d87d54c29581f839231ed2083b250054d83250a
GrEugene <eugene7007@yandex.com>
2020-01-09 13:04:25 +0500

    Add sout info

--- src/main/java/com/helpdesk/crm/Main.java
+++ src/main/java/com/helpdesk/crm/Main.java
@@ -3,6 +3,7 @@ package com.helpdesk.crm;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("This is main method");
         System.out.println("Hello World");
     }
 }
