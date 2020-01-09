5a57b555ab447406eea0bd8ecc9a8875eec0829f
GrEugene <eugene7007@yandex.com>
2020-01-09 14:15:00 +0500

    sdasd

--- src/main/java/com/helpdesk/crm/Main.java
+++ src/main/java/com/helpdesk/crm/Main.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
     public static final String COMPANY_NAME = "Aurora";
+    public static final String COMPANY_ADDRESS = "Aurora";
 
     public static void main(String[] args) {
         log.info("This is main method");
