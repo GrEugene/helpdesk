0a053490b9d88746d2f10c5be7192c4938569d51
GrEugene <eugene7007@yandex.com>
2020-01-09 12:59:59 +0500

    Add log into main method

--- src/main/java/com/helpdesk/crm/Main.java
+++ src/main/java/com/helpdesk/crm/Main.java
@@ -1,8 +1,12 @@
 package com.helpdesk.crm;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
+        log.info("This is main method");
         System.out.println("Hello World");
     }
 }
